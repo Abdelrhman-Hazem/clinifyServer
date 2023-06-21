1fec6834e36790bc0d9fb4c4d23ae0d7c7daacd6
Abdelrahman Hazem <abdelrhman.hazem.01@gmail.com>
2023-06-21 21:05:41 +0300

    adding annotation on controllers

--- src/main/java/gov/iti/jets/clinify/controllers/PatientDocumentController.java
+++ src/main/java/gov/iti/jets/clinify/controllers/PatientDocumentController.java
@@ -2,10 +2,12 @@ package gov.iti.jets.clinify.controllers;
 
 import gov.iti.jets.clinify.models.dtos.PatientDocumentDto;
 import gov.iti.jets.clinify.models.entities.PatientDocument;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/patientDocuments")
+@CrossOrigin
 public class PatientDocumentController extends BaseController<PatientDocument, PatientDocumentDto> {
 }
