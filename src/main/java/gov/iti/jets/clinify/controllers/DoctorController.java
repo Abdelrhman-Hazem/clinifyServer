3967f6f1d45b3311ec4af1349668ba48c7d7a2d5
Abdelrahman-Amr <abdozyed2199@gmail.com>
2023-06-22 20:43:05 +0300

    added upload and download images and application profiles

--- src/main/java/gov/iti/jets/clinify/controllers/DoctorController.java
+++ src/main/java/gov/iti/jets/clinify/controllers/DoctorController.java
@@ -9,9 +9,25 @@ import gov.iti.jets.clinify.utils.MessageResponse;
 import gov.iti.jets.clinify.utils.PageQueryUtil;
 import gov.iti.jets.clinify.utils.PageResult;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import static java.nio.file.Files.copy;
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
+
 
 @RestController
 @RequestMapping("/doctors")
@@ -48,5 +64,32 @@ public class DoctorController extends BaseController<Doctor, DoctorDto> {
         PageQueryUtil queryUtil = new PageQueryUtil(page, limit);
         return doctorService.getDoctorsDataPage(queryUtil);
     }
+
+    public static final String DIRECTORY =
+            System.getProperty("user.dir") + "/src/main/resources/images/doctorImages/";
+
+    @PostMapping("/upload")
+    public ResponseEntity<List<String>> uploadFiles(@RequestParam("files")List<MultipartFile> multipartFiles) throws IOException {
+        System.out.println("hiii");
+        List<String> filenames = new ArrayList<>();
+        for(MultipartFile file : multipartFiles) {
+            String filename = StringUtils.cleanPath(Objects.requireNonNull(file.getOriginalFilename()));
+            Path fileStorage = Paths.get(DIRECTORY, filename).toAbsolutePath().normalize();
+            copy(file.getInputStream(), fileStorage, REPLACE_EXISTING);
+            filenames.add(filename);
+        }
+        return ResponseEntity.ok().body(filenames);
+    }
+
+    @GetMapping("/download/{filename}")
+    public ResponseEntity<Resource> downloadFiles(@PathVariable("filename") String filename) throws IOException {
+        Path filePath = Paths.get(DIRECTORY).toAbsolutePath().normalize().resolve(filename);
+        if(!Files.exists(filePath)) {
+            throw new FileNotFoundException(filename + " was not found on the server");
+        }
+        Resource resource = new UrlResource(filePath.toUri());
+        return new ResponseEntity<>(resource,HttpStatus.OK);
+    }
+
 
 }
